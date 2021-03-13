9a6621e750d437c29457b270030dc9b194d45082
Zabiulla9 <61347712+Zabiulla9@users.noreply.github.com>
2021-03-13 15:18:32 +0530

    Update ansi.sh

--- Terraform_code_java_proj/ansi.sh
+++ Terraform_code_java_proj/ansi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt install tree ansible -y
-sudo touch /home/ubuntu/Zabiulla.pem
 cd /home/ubuntu/
 sudo git clone https://github.com/Zabiulla9/Terraform-project.git
