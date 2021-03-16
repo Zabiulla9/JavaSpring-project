ed22b2af2a7d6161168f9443afcd8336ca3b508c
Zabiulla9 <61347712+Zabiulla9@users.noreply.github.com>
2021-03-16 16:15:31 +0530

    Update sg_module.tf

--- Terraform_code_java_proj/sg_module/sg_module.tf
+++ Terraform_code_java_proj/sg_module/sg_module.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "git_dock_jen" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 8080
